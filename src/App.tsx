b01385270c8cfb057551574aa96b82cc04c5a0c3
zaakirio <zaakir.ally@pm.me>
2025-03-16 17:23:42 +0000

    added more debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { AudioProvider } from './contexts/AudioContext';
 import Game from './components/Game';
+import './services/ElevenLabsClient';
+import './services/AudioCacheService';
 
 function App() {
   return (
@@ -10,5 +12,19 @@ function App() {
     </AudioProvider>
   );
 }
+function checkApiConnectivity() {
+  if (import.meta.env.PROD) {
+    // Test ElevenLabs connectivity
+    fetch('https://api.elevenlabs.io/v1/voices')
+      .then(response => {
+        document.body.setAttribute('data-elevenlabs-reachable', response.ok.toString());
+      })
+      .catch(error => {
+        document.body.setAttribute('data-elevenlabs-error', error.toString());
+      });
+  }
+}
 
+// Call this early in your app initialization
+checkApiConnectivity();
 export default App;
